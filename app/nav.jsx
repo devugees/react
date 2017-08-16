04163e552fcce783d3da8fd562ebdb4b4a12923c
ayhamk94 <ayham.kteash1995@gmail.com>
2017-08-16 10:01:06 +0200

    new styels for the website

--- app/nav.jsx
+++ app/nav.jsx
@@ -6,7 +6,7 @@ class Nav extends React.Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a classNameName="navbar-brand" href="#">Navbar</a>
+          <a classNameName="navbar-brand" href="#"> <span><img src="imgs/logo.png" id="logo"/></span></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -14,18 +14,12 @@ class Nav extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home
+                <a className="nav-link" href="#">Stonergees
                   <span className="sr-only">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link " data-toggle="modal" data-target="#exampleModalLong" href="#">Modal</a>
+                <a className="nav-link " data-toggle="modal" data-target="#exampleModalLong" href="#">Cart</a>
               </li>
             </ul>
             <form className="form-inline my-2 my-lg-0">
