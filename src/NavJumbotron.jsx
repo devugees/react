76ec5bd7f8419b9ebfcafe081624a03fd4b6e875
Felix Schaumann <felix.schaumann@annalect.com>
2017-08-17 09:24:17 +0200

    change setup from gulp+webpack to create-react-app

--- src/NavJumbotron.jsx
+++ src/NavJumbotron.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 
 const NavJumbotron = () => {
   return (
-
       <div className="jumbotron jumbotron-fluid bg-transparent">
         <h1 className="display-3"> Welcome to Stonergees!</h1>
-        <p className="lead">Here you will find all the products we are offering.</p>
-        <p>Feel free  to explore all the sections.</p>
+        <p className="lead">Here you will find all the products we are offering. Feel free  to explore all the sections.</p>
       </div>
-
   )
 }
 
